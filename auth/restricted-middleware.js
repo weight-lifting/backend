dadc51b411bb58cfab59546cbe691124d07653e6
milkslapper <oscar.valenzuela69@gmail.com>
2019-09-06 21:03:13 -0700

    created databases and added auth and user router

---
+++ auth/restricted-middleware.js
@@ -0,0 +1,22 @@
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
+const secrets = require('../config/secrets');
+const Users = require('../users/users-model.js');
+
+module.exports = (req, res, next) => {
+    const token = req.headers.authorization;
+
+    if (token) {
+        jwt.verify(token, secrets.jwt, (err, payload) => {
+            if (err) {
+                res.status(403).json({ message: 'you are not authorized' });
+            } else {
+                req.userId = payload.userId;
+                next();
+            }
+        })
+    } else {
+        res.status(400).json({ message: 'No credentials provided' });
+    }
+};
