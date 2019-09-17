451cf9482174d3db41c2a5a806d2119ac1bc145e
Poly Goveia <poly.goveia@gmail.com>
2019-09-16 23:23:43 -0700

    update test and exercise

--- exercises/exercise-model.spec.js
+++ exercises/exercise-model.spec.js
@@ -1,120 +1,195 @@
-const db = require("../data/config")
+const db = require("../data/config");
 const Exercises = require("./exercises-model");
 
+describe("exercises model", () => {
+  describe("add", () => {
+    afterEach(async () => {
+      //clean up
+      await db("exercises").truncate();
+    });
+    it("should insert a exercise into the db", async () => {
+      //using our model method
+      await Exercises.add({
+        id: 0,
+        title: "planks",
+        reps_completed: 1,
+        date: "2020-01-15",
+        targeted_area: "neck",
+        user_id: 0
+      });
+      await Exercises.add({
+        id: 1,
+        title: "planksss",
+        reps_completed: 11,
+        date: "2020-01-13",
+        targeted_area: "brain",
+        user_id: 1
+      });
 
-describe('exercises model',() => {
+      //confirm with knex
+      const exercises = await db("exercises");
 
-    describe('add', () => {
-        
-        
-        afterEach(async () => {
-            //clean up
-            await db('exercises').truncate();
-        })
-        it("should insert a exercise into the db", async () => {
-            //using our model method
-            await Exercises.add({ id: 0,title: "planks", reps_completed: 1 ,date:"2020-01-15",targeted_area:"neck",user_id:0})
-            await Exercises.add({ id: 1,title: "planksss", reps_completed: 11,date:"2020-01-13",targeted_area:"brain",user_id:1})
+      expect(exercises).toHaveLength(2);
+    });
+    it("should return new exercise on insert", async () => {
+      const exercise = await Exercises.add({
+        id: 1,
+        title: "planks",
+        reps_completed: 1,
+        date: "2020-01-15",
+        targeted_area: "neck",
+        user_id: 0
+      });
 
-            //confirm with knex
-            const exercises = await db('exercises');
-            
-            expect(exercises).toHaveLength(2);
-        })
-        it("should return new exercise on insert", async () => {
-            const exercise = await Exercises.add({ id: 1,title: "planks", reps_completed: 1,date:"2020-01-15",targeted_area:"neck",user_id:0})
-            
-            expect(exercise).toEqual({ id: 1,title: "planks", reps_completed: 1 ,date:"2020-01-15",targeted_area:"neck",user_id:0})
-        })
-    })
+      expect(exercise).toEqual({
+        id: 1,
+        title: "planks",
+        reps_completed: 1,
+        date: "2020-01-15",
+        targeted_area: "neck",
+        user_id: 0
+      });
+    });
+  });
 
-    describe('findById', () => {
-       
-        afterEach(async () => {
-            //clean up
-            await db('exercises').truncate();
-        })
-    
-        it('finds a exercise by id', async () => {
-            //set up
-            await Exercises.add({ id: 0,title: "planks", reps_completed: 1 ,date:"2020-01-15",targeted_area:"neck",user_id:0})
-            await Exercises.add({ id: 1,title: "planksss", reps_completed: 11,date:"2020-01-13",targeted_area:"brain",user_id:1})
+  describe("findById", () => {
+    afterEach(async () => {
+      //clean up
+      await db("exercises").truncate();
+    });
 
-    
-            const exercises = await Exercises.findById(2);
-    
-            expect (exercises).toBe()
-        })
+    it("finds a exercise by id", async () => {
+      //set up
+      await Exercises.add({
+        id: 0,
+        title: "planks",
+        reps_completed: 1,
+        date: "2020-01-15",
+        targeted_area: "neck",
+        user_id: 0
+      });
+      await Exercises.add({
+        id: 1,
+        title: "planksss",
+        reps_completed: 11,
+        date: "2020-01-13",
+        targeted_area: "brain",
+        user_id: 1
+      });
 
-        it('returns undefined on invalid id', async () => {
-            const exercises = await Exercises.findById(2);
+      const exercises = await Exercises.findById(2);
 
-            expect(exercises).toBeUndefined();
-        })
-    })
+      expect(exercises).toBe();
+    });
 
-    describe('find',() => {
-        afterEach(async () => {
-            //clean up
-            await db('exercises').truncate();
-        })
-        it("should return all exercises", async () => {
-            //using our model method
-            await Exercises.add({ id: 0,title: "planks", reps_completed: 1 ,date:"2020-01-15",targeted_area:"neck",user_id:0})
-            await Exercises.add({ id: 1,title: "planksss", reps_completed: 11,date:"2020-01-13",targeted_area:"brain",user_id:1})
+    it("returns undefined on invalid id", async () => {
+      const exercises = await Exercises.findById(2);
 
+      expect(exercises).toBeUndefined();
+    });
+  });
 
-            //confirm with knex
-            const exercises = await Exercises.find();
-            expect(exercises).toHaveLength(2);
-        })
-    })
+  describe("find", () => {
+    afterEach(async () => {
+      //clean up
+      await db("exercises").truncate();
+    });
+    it("should return all exercises", async () => {
+      //using our model method
+      await Exercises.add({
+        id: 0,
+        title: "planks",
+        reps_completed: 1,
+        date: "2020-01-15",
+        targeted_area: "neck",
+        user_id: 0
+      });
+      await Exercises.add({
+        id: 1,
+        title: "planksss",
+        reps_completed: 11,
+        date: "2020-01-13",
+        targeted_area: "brain",
+        user_id: 1
+      });
 
-    describe("remove", () => {
-        afterEach(async () => {
-            //clean up
-            await db("exercises").truncate();
-        })
-        it('removes specific exercise', async () => {
-            //using our model method
-            await Exercises.add({ id: 0,title: "planks", reps_completed: 1 ,date:"2020-01-15",targeted_area:"neck",user_id:0})
-            await Exercises.add({ id: 1,title: "planksss", reps_completed: 11,date:"2020-01-13",targeted_area:"brain",user_id:1})
+      //confirm with knex
+      const exercises = await Exercises.find();
+      expect(exercises).toHaveLength(2);
+    });
+  });
 
-            //confirm with knex
-            
+  describe("remove", () => {
+    afterEach(async () => {
+      //clean up
+      await db("exercises").truncate();
+    });
+    it("removes specific exercise", async () => {
+      //using our model method
+      await Exercises.add({
+        id: 0,
+        title: "planks",
+        reps_completed: 1,
+        date: "2020-01-15",
+        targeted_area: "neck",
+        user_id: 0
+      });
+      await Exercises.add({
+        id: 1,
+        title: "planksss",
+        reps_completed: 11,
+        date: "2020-01-13",
+        targeted_area: "brain",
+        user_id: 1
+      });
 
-            await Exercises.remove(1)
-            const exercises = await db('exercises')
-            expect(exercises).toHaveLength(1)
-        })
-    })
+      //confirm with knex
 
-    describe("update", () => {
-        afterEach(async () => {
-            //clean up 
-            await db("exercises").truncate();
-        })
-        it("updates specific exercises", async () => {
-            //using our model method
-           
-            const exercise_1 = {
-                id: 0,title: "planks", reps_completed: 1 ,date:"2020-01-15",targeted_area:"neck",user_id:0
-            }
-            const exercise_2 = {
-                id: 1,title: "planksss", reps_completed: 11,date:"2020-01-13",targeted_area:"brain",user_id:1
-            }
+      await Exercises.remove(1);
+      const exercises = await db("exercises");
+      expect(exercises).toHaveLength(1);
+    });
+  });
 
-            await Exercises.add(exercise_1)
-            await Exercises.add(exercise_2)
-            //confirm with knex
-            const changes = {
-                id: 1,title: "pl", reps_completed: 1,date:"2020-01-1",targeted_area:"br",user_id:1
-            }
-            await  Exercises.update(1,changes)
-            const exercises = await Exercises.findById(1)
-            expect(exercises.id).toEqual(changes.id)
-        })
-        
-    })
+  describe("update", () => {
+    afterEach(async () => {
+      //clean up
+      await db("exercises").truncate();
+    });
+    it("updates specific exercises", async () => {
+      //using our model method
 
+      const exercise_1 = {
+        id: 0,
+        title: "planks",
+        reps_completed: 1,
+        date: "2020-01-15",
+        targeted_area: "neck",
+        user_id: 0
+      };
+      const exercise_2 = {
+        id: 1,
+        title: "planksss",
+        reps_completed: 11,
+        date: "2020-01-13",
+        targeted_area: "brain",
+        user_id: 1
+      };
+
+      await Exercises.add(exercise_1);
+      await Exercises.add(exercise_2);
+      //confirm with knex
+      const changes = {
+        id: 1,
+        title: "pl",
+        reps_completed: 1,
+        date: "2020-01-1",
+        targeted_area: "br",
+        user_id: 1
+      };
+      await Exercises.update(changes, 1);
+      const exercises = await Exercises.findById(1);
+      expect(exercises.id).toEqual(changes.id);
+    });
+  });
 });
